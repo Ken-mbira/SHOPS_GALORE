8f3cf79419fa5b1631e263c01f5814d10863774c
ken <ken.mbira@student.moringaschool.com>
2021-12-08 09:17:14 +0300

    correct improper user import in signals

--- apps/account/signals.py
+++ apps/account/signals.py
@@ -1,5 +1,4 @@
 from apps.account.models import *
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
