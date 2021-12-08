2529a76097af008c89060a00330d2f49deebf2e1
ken <ken.mbira@student.moringaschool.com>
2021-12-08 13:42:57 +0300

    implement account activation via email functionality

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from apps.account.models import *
+
+admin.site.register(User)
+
 # Register your models here.
--- apps/account/urls.py
+++ apps/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',views.UserView.as_view(),name="create_user"),
     path('login/',views.LoginView.as_view(),name="login"),
     path('login/<token>',views.UserInstanceView.as_view(),name="user_instance"),
+    path('activate/<uidb64>/<token>/', views.ActivateAccount.as_view(), name='activate'),
 ]
