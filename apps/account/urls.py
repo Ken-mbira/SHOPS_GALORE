52d527e1d36d48ece1aa22fe757da4eee2e21745
ken <ken.mbira@student.moringaschool.com>
2021-12-08 09:35:11 +0300

    add login endpoint

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from apps.account import views
 
 urlpatterns = [
-    path('',views.UserView.as_view(),name="create_user")
+    path('',views.UserView.as_view(),name="create_user"),
+    path('login/',views.LoginView.as_view(),name="login"),
 ]
--- apps/account/views.py
+++ apps/account/views.py
@@ -4,6 +4,7 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.schemas import get_schema_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from apps.account.serializers import *
 from apps.account.models import *
@@ -26,3 +27,28 @@ class UserView(APIView):
             data = serializer.errors
             responseStatus = status.HTTP_400_BAD_REQUEST
         return Response(data,responseStatus)
+
+class LoginView(APIView):
+    """This handles a user login request
+
+    Args:
+        APIView ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    @swagger_auto_schema(request_body=LoginSerializer,responses={200: GetUserSerializer()})
+    def post(self,request,format=None):
+        data = {}
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validate_user()
+            data['user'] = GetUserSerializer(user).data
+            token, created = Token.objects.get_or_create(user=user)
+            data['token'] = token.key
+            responseStatus = status.HTTP_200_OK
+            return Response(data,status = responseStatus)
+
+        else:
+            data = serializer.errors
+            return Response(data,status = status.HTTP_400_BAD_REQUEST)
