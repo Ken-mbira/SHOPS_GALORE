90d0726500e6adc8d3d52e1f7712a4b2e7f8b563
ken <ken.mbira@student.moringaschool.com>
2021-12-11 10:30:37 +0300

    modify shop module

--- apps/store/serializers.py
+++ apps/store/serializers.py
@@ -17,3 +17,35 @@ class ShopSerializer(serializers.ModelSerializer):
         shop = Shop(name = self.validated_data['name'],bio = self.validated_data['bio'],owner = request.user,pickup_location = self.validated_data['pickup_location'],phone_contact = self.validated_data['phone_contact'],email_contact = self.validated_data['email_contact'])
         shop.save()
         return shop
+
+    def update(self,instance):
+        instance.name = self.validated_data['name']
+        instance.bio = self.validated_data['bio']
+        instance.pickup_location = self.validated_data['pickup_location']
+        instance.phone_contact = self.validated_data['phone_contact']
+        instance.email_contact = self.validated_data['email_contact']
+        instance.save()
+        return instance
+        
+class UpdateShopSerializer(serializers.Serializer):
+    """This handles shop updating
+
+    Args:
+        serializers ([type]): [description]
+    """
+    shop = serializers.CharField(required=True)
+
+    def delete(self):
+        try:
+            instance = Shop.objects.get(pk = int(self.validated_data['shop']))
+        except:
+            raise serializers.ValidationError("The object was not found")
+
+        instance.deactivate()
+
+    def validate_exists(self):
+        try:
+            Shop.objects.get(pk = int(self.validated_data['shop']))
+            return True
+        except:
+            raise serializers.ValidationError("The object was not found")
