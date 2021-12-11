b83ac1251b2c62fe564f2ecb713096fd43d5b679
ken <ken.mbira@student.moringaschool.com>
2021-12-11 12:36:46 +0300

    add test associated with deleting a shop

--- apps/store/tests/test_views.py
+++ apps/store/tests/test_views.py
@@ -200,3 +200,89 @@ class TestShopViews(TestShop):
         response = self.client.put(reverse('update',kwargs={"id":100}),new_shop_profile)
 
         self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+
+    def test_delete_shop(self):
+        """This will test if a user can delete their own shop
+        """
+
+        self.client.post(self.register_url,self.user_data)
+
+        user = User.objects.get(email = self.login_credentials['email'])
+        user.is_active = True
+        user.save()
+
+        self.authenticate(self.login_credentials)
+
+        created_shop = self.client.post(self.create_shop_url,self.shop_details)
+        
+        response = self.client.delete(reverse('update',kwargs={"id":created_shop.data['id']}))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(False,(Shop.objects.get(pk = created_shop.data['id']).functional and Shop.objects.get(pk = created_shop.data['id']).active))
+
+    def test_delete_others_shop(self):
+        """This test checks if a user can update a shop that's not his'hers
+        """
+        self.client.post(self.register_url,self.user_data)
+
+        user = User.objects.get(email = self.login_credentials['email'])
+        user.is_active = True
+        user.save()
+
+        shop = Shop(
+            name = self.shop_details['name'],
+            bio = self.shop_details['bio'],
+            owner = User.objects.get(email = self.login_credentials['email']),
+            pickup_location = self.shop_details['pickup_location'],
+            email_contact = self.shop_details['email_contact'],
+        )
+        shop.save()
+
+        other_user_credentials = {
+            "password":"1234",
+            "first_name":"Musa",
+            "last_name":"Mosomi",
+            "email":"mosomi@gmail.com",
+            "role":2
+        }
+
+        other_user_login_credentials = {
+            "email":other_user_credentials['email'],
+            "password":other_user_credentials['password']
+        }
+
+        self.client.post(self.register_url,other_user_credentials)
+
+        user = User.objects.get(email = other_user_login_credentials['email'])
+        user.is_active = True
+        user.save()
+
+        self.authenticate(other_user_login_credentials)
+
+        new_shop_profile = {
+            "name": "Maendeleo developers",
+            "bio": "A new company slogan",
+            "pickup_location": "string",
+            "phone_contact": "+254722442604",
+            "email_contact": "mbira@ken.com"
+        }
+
+        response = self.client.delete(reverse('update',kwargs={"id":shop.pk}),new_shop_profile)
+
+        self.assertEqual(response.status_code,status.HTTP_403_FORBIDDEN)
+
+    def test_delete_non_existent_shop(self):
+        """This will test if a user can delete their own shop
+        """
+
+        self.client.post(self.register_url,self.user_data)
+
+        user = User.objects.get(email = self.login_credentials['email'])
+        user.is_active = True
+        user.save()
+
+        self.authenticate(self.login_credentials)
+
+        created_shop = self.client.post(self.create_shop_url,self.shop_details)
+        
+        response = self.client.delete(reverse('update',kwargs={"id":10000}))
+        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
