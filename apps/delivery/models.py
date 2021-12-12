ec2ede1d3ef97bb115373b91bf71673fd3f2db87
ken <ken.mbira@student.moringaschool.com>
2021-12-12 16:57:01 +0300

    update create shop functionality to cater for the shop instance

--- apps/delivery/models.py
+++ apps/delivery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+class Location(MPTTModel):
+    """This designs how the location will be
+
+    Args:
+        MPTTModel ([type]): [description]
+    """
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey("self",on_delete=models.PROTECT,related_name="children",null=True,blank=True)
+
+    def __str__(self):
+        return self.name
