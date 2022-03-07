04de56da46030af7982b9edaf5ed0d75e2d20685
ken <ken.mbira@student.moringaschool.com>
2022-03-07 16:22:01 +0300

    remove redundant serializers from account

--- apps/account/serializers.py
+++ apps/account/serializers.py
@@ -1,13 +1,9 @@
 from multiprocessing import AuthenticationError
-from django.forms import ValidationError
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from django.contrib.sites.shortcuts import get_current_site
 from phonenumber_field.modelfields import PhoneNumberField
 from decouple import config
 
 from apps.account.models import *
-from apps.account.emails import send_account_activation_email
 from apps.storage.models import *
 from apps.account import google
 from apps.account import facebook
@@ -32,44 +28,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
-
-class RegisterStaffSerializer(serializers.ModelSerializer):
-    """This involves creation of a new staff member
-
-    Args:
-        serializers ([type]): [description]
-
-    Raises:
-        serializers.ValidationError: [description]
-        serializers.ValidationError: [description]
-
-    Returns:
-        [type]: [description]
-    """
-    storage_facility = serializers.IntegerField(required=True)
-    class Meta:
-        model = User
-        fields = ['password','email','role','first_name','last_name','storage_facility']
-
-    def save(self,request):
-        storage_facility = StorageFacility.objects.get(pk=self.validated_data['storage_facility'])
-        user = User(first_name = self.validated_data['first_name'], last_name=self.validated_data['last_name'],email=self.validated_data['email'],role=self.validated_data['role'])
-        user.set_password(self.validated_data['password'])
-        user.save()
-        staff_profile = StaffProfile.objects.get(user = user)
-        staff_profile.storage_facility = storage_facility
-        staff_profile.save()
-        return user
-
-class RoleSerializer(serializers.ModelSerializer):
-    """This deals with serializing the user roles
-
-    Args:
-        serializers ([type]): [description]
-    """
-    class Meta:
-        model = Role
-        fields = '__all__'
 
 class ProfileSerializer(serializers.ModelSerializer):
     """This serializes the profile model
@@ -97,90 +55,11 @@ class GetUserSerializer(serializers.ModelSerializer):
     Args:
         serializers ([type]): [description]
     """
-    role = RoleSerializer()
     profile = ProfileSerializer()
     class Meta:
         model = User
         fields = ['email','role','first_name','last_name','member_since','profile']
 
-
-class LoginSerializer(serializers.Serializer):
-    """This defines the functions in the login function
-
-    Args:
-        serializers ([type]): [description]
-
-    Raises:
-        serializers.ValidationError: [description]
-    """
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(write_only=True,required=True)
-
-    def validate_user(self):
-        """This handles the validation of the user
-
-        Raises:
-            serializers.ValidationError: [description]
-        """
-        user = authenticate(email = self.validated_data['email'],password = self.validated_data['password'])
-
-        if user is not None:
-            return user
-
-        else:
-            raise serializers.ValidationError('The user could not be validated with the provided credentials.')
-
-class NotificationPreferenceSerializer(serializers.Serializer):
-    preference = serializers.BooleanField(required=True,label="The user's preference")
-
-    def change_preference(self,request):
-        profile = Profile.objects.get(user = request.user)
-        profile.receive_notifications_via_email = self.validated_data['preference']
-        profile.save()
-        return profile
-
-class ProfilePicSerializer(serializers.Serializer):
-    """This handles updating the profile pic of a user
-
-    Args:
-        serializers ([type]): [description]
-    """
-    avatar = serializers.ImageField(required=True)
-
-    def update_profile_pic(self,request):
-        profile = Profile.objects.get(user = request.user)
-        profile.avatar = self.validated_data['avatar']
-        profile.save()
-        return profile
-
-class AccountStatusSerializer(serializers.Serializer):
-    """This handles activating or deactivating another users account
-
-    Args:
-        serializers ([type]): [description]
-    """
-    user = serializers.CharField(required=True)
-
-    def validate_instance(self):
-        """This validates if the user instance is valid
-        """
-        try:
-            User.objects.get(pk = self.validated_data['user'])
-            return True
-        except:
-            raise serializers.ValidationError("The user instance could not be found")
-
-    def deactivate_user(self):
-        """this handles deactivation of a user's account
-        """
-        user = User.objects.get(pk = self.validated_data['user'])
-        user.deactivate_account()
-
-    def reinstate_user(self):
-        """This handles the reinstation of a user
-        """
-        user = User.objects.get(pk = self.validated_data['user'])
-        user.reinstate()
 
 class SocialLoginSerializer(serializers.Serializer):
     """This handles the user logging in with one of the social login methods
