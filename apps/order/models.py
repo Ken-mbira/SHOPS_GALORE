b00b309ab235d20657897d3336231af5a510523e
ken <ken.mbira@student.moringaschool.com>
2022-03-12 09:14:18 +0300

    prep for deployment

--- apps/order/models.py
+++ apps/order/models.py
@@ -28,7 +28,6 @@ class Cart(models.Model):
             self.token  = uuid.uuid4()
         super().save()
 
-        # Hello
 
 class CartItem(models.Model):
     """This is one instance of a product within a cart
